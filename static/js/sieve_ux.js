4934986f3aaee44e865c0ec91816b46d4e816017
Thibauld Nion <thibauld@tibonihoo.net>
2013-05-05 22:29:40 +0200

    ajax interaction to mark items as read + little bits of refactoring

--- static/js/sieve_ux.js
+++ static/js/sieve_ux.js
@@ -13,15 +13,21 @@ function prepareKeyBindings()
   gMouseTrapDisabled = true;
   gCurrentlyExpandedItem = -1;
   gNumReferences = 0;
+  gSyncWithServer = false;
+  gReadURLs = [];
 }
 
+
 // Activation that needs to be called once the page is fully generated
-function activateKeyBindings()
+// @param syncWithServer a boolean telling whether the read status
+// should be synced with the server.
+function activateKeyBindings(syncWithServer)
 {
   // keybindings globals
   gCurrentlyExpandedItem = -1;
   gMouseTrapDisabled = false;   
   gNumReferences = $(".reference").length;
+  gSyncWithServer = syncWithServer;
   // hook the hide/show calbacks in the feed items
   for(idx=0;idx<gNumReferences;idx+=1)
   {
@@ -31,8 +37,27 @@ function activateKeyBindings()
   }
 }
 
+// Show one of the warning that are written but hidden by default on
+// the page, and that are identified via their HTML id.
+// WARNING: if there are several warnings, they may overlap (TODO: in
+// such case use a warning counter + a shift computed accordingly)
+function showWarning(warningId) {
+  var warningElt = $("#"+warningId);
+  warningElt.popover();
+  warningElt.css({ 
+    "display": "block",
+    "position": "absolute",
+    "left": "3px",
+    "top": $(window).height()/3.});
+}
+
+// Hide a specific warning.
+function hideWarning(warningId) {
+  $("#"+warningId).css("display", "none");
+}
+
 // Make sure that an element is visible by scrolling the page if necessary to make it appear in the first 3 quarters of the page.
-// adapted from http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling 
+// Note: adapted from http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling 
 function ensureVisibilityInFirstThreeQuarters(elem)
 {
   var windowHeight = $(window).height();
@@ -50,17 +75,8 @@ function ensureVisibilityInFirstThreeQuarters(elem)
   }
 }
 
-
-// adapted from http://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
-function createHiddenCallbackFunc(i) {
-  return function() { 
-    if (!gMouseTrapDisabled) 
-    {
-      gCurrentlyExpandedItem = -1;
-    }  
-  };
-}
-
+// Generate the callback that will be called when the content of
+// reference#i will be collapsed.
 function createShownCallbackFunc(i) {
   return function() { 
     if (!gMouseTrapDisabled) 
@@ -76,6 +92,90 @@ function createShownCallbackFunc(i) {
 }
 
 
+// Generate the callback that will be called when the content of
+// reference#i will be collapsed.
+// Note: adapted from http://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
+function createHiddenCallbackFunc(i) {
+  return function() { 
+    if (!gMouseTrapDisabled) 
+    {
+      gCurrentlyExpandedItem = -1;
+    }  
+  };
+}
+
+// using jQuery to get a cookie 
+// (from https://docs.djangoproject.com/en/dev/ref/contrib/csrf/)
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+// Django-specific method to distinguish which request needs to take
+// care of the crsf protection
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+// Make sure the server will know that certain items have been read
+// @param read_items_urls a list of urls identifying the references
+// that must be considered as read.
+// @param callback function to be called when the server's answer is received
+function updateReadStatusOnServer(read_items_urls, callback) {
+  if (!gSyncWithServer) return;
+  var jsonStr = JSON.stringify({"action": "read","references":read_items_urls})
+  var csrftoken = getCookie('csrftoken');
+  var currentURL = window.location.href;
+  $.ajax({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+      if (!csrfSafeMethod(settings.type)) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    },
+    type: "POST",
+    url: currentURL,
+    data: jsonStr,
+    dataType: "json",
+  }).done(callback).fail(function () {showWarning("server-sync-problem");});
+  // }).done(callback).fail(function(j,t) {alert(t);} );
+}
+
+
+
+// Perform all necessary stuff to indicate that a reference should be
+// considered as read.
+// Note: will act only if the reference has not be marked as read yet !
+// @param refElement the element representing the reference that must
+// be marked as read
+// @param refIdx the index of this reference (typically as indicated
+// in #reference{refIdx)
+function markAsRead(refElement,refIdx) {
+  if ( !refElement.hasClass('read') ) { 
+    refElement.addClass("read") 
+    gReadURLs.push($('#referenceUrl'+refIdx.toString()).prop("href"));
+    // we still upload the full *current* list of read items (if
+    // there's more than one it probably means that something went
+    // wrong with the latest update)
+    updateReadStatusOnServer(gReadURLs,function (data) {gReadURLs = []; hideWarning("server-sync-problem");});
+  };
+}
+
+
+// Keybinding activation
+
 // Expand previous item
 Mousetrap.bind('p', function() { 
   if(gMouseTrapDisabled) {return false;}
@@ -84,7 +184,7 @@ Mousetrap.bind('p', function() {
   var itemToCollapse = 'collapse'+currentIdx.toString();
   $('#'+itemToCollapse).collapse('hide');
   var referenceId = '#reference'+currentIdx.toString()
-  if ( !$(referenceId).hasClass('read') ) { $(referenceId).addClass("read") }; 
+  markAsRead($(referenceId),currentIdx);
   if (gCurrentlyExpandedItem <= 0)
   {
     if (gCurrentlyExpandedItem == 0) { gCurrentlyExpandedItem = -1;}
@@ -106,7 +206,10 @@ Mousetrap.bind('n', function() {
   var itemToCollapse = 'collapse'+currentIdx.toString();
   $('#'+itemToCollapse).collapse('hide');
   var referenceId = '#reference'+currentIdx.toString()
-  if ( !$(referenceId).hasClass('read') ) { $(referenceId).addClass("read") }; 
+  if ( currentIdx>-1 ) 
+  { 
+    markAsRead($(referenceId),currentIdx);
+  }
   if (gCurrentlyExpandedItem >= gNumReferences - 1)
   {
     if (gCurrentlyExpandedItem == gNumReferences - 1) { gCurrentlyExpandedItem = gNumReferences }
@@ -125,4 +228,12 @@ Mousetrap.bind('v', function() {
   var currentIdx = gCurrentlyExpandedItem;
   var itemToShow = '#referenceUrl'+currentIdx.toString();
   window.open($(itemToShow).prop("href"));
+});
+
+// open the currently expanded items' linked page in the browser
+Mousetrap.bind('r', function() { 
+  var window_location = window.location;
+  updateReadStatusOnServer(gReadURLs,function (data) {gReadURLs = []; window_location.reload();});
+  // TODO apparently there is a need for a little timeout to get a
+  // coherent refresh of the page !
 });
