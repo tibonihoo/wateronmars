2c40e3d9716f64736e2d026317b4f73a6b6e36f1
Thibauld Nion <thibauld@tibonihoo.net>
2013-05-05 22:26:55 +0200

    rework views and handle POST for read items in sieve

--- wom_river/views.py
+++ wom_river/views.py
@@ -1,10 +1,17 @@
 from django.template import Context, loader
 from django.http import HttpResponse
+from django.http import HttpResponseNotAllowed
+from django.http import HttpResponseBadRequest
+from django.utils import simplejson
+from django.db import transaction
 
 from django.contrib.auth.decorators import login_required
 
 from wom_pebbles.models import Reference
 from wom_river.models import FeedSource
+from wom_river.models import ReferenceUserStatus
+from wom_river.tasks import check_user_unread_feed_items
+
 
 MAX_ITEMS_PER_PAGE = 100
 
@@ -91,20 +98,23 @@ def user_river_view(request):
       })
   return HttpResponse(t.render(c))
 
-@login_required(login_url='/accounts/login/')
-def user_river_sieve(request):
-  user_profile = request.user.userprofile
-  latest_unread_pebbles = []
-  for source in user_profile.feed_sources.all():
-    latest_unread_pebbles.extend(source.reference_set.order_by('pub_date')[:MAX_ITEMS_PER_PAGE])
-  latest_unread_pebbles.sort(key=lambda x:x.pub_date)
+  
+
+
+def generate_user_sieve(request):
+  """
+  Generate the HTML page on which a given user will be able to see and
+  use it's sieve to read and sort out the latests news.
+  """
+  check_user_unread_feed_items(request.user)
+  latest_unread_pebbles = ReferenceUserStatus.objects.filter(has_been_read=False).order_by('ref_pub_date')[:MAX_ITEMS_PER_PAGE]
   t = loader.get_template('wom_river/sieve.html_dt')
   if not latest_unread_pebbles:
     messages = ["No pebble yet !"]
   else:
     messages = []
   c = Context({
-      'latest_unread_pebbles': latest_unread_pebbles,
+      'latest_unread_pebbles': [ rust.ref for rust in latest_unread_pebbles],
       'messages' : messages,
       'username' : request.user.username,
       'num_unread_pebbles': len(latest_unread_pebbles),
@@ -112,7 +122,49 @@ def user_river_sieve(request):
       'realm': "u/%s" % request.user.username,
       })
   return HttpResponse(t.render(c))
+  
+def apply_to_user_sieve(request):
+  """
+  Act on the items passing through the sieve.
+  
+  The only accepted action for now is to mark items as read, with the
+  following JSON payload::
+  
+    { action = "read",
+      references = [ "<url1>", "<url2>", ...],
+    }
+  """
+  check_user_unread_feed_items(request.user)
+  print request.body
+  action_dict = simplejson.loads(request.body)
+  print action_dict
+  if action_dict.get(u"action") != u"read":
+    return HttpResponseBadRequest("Only a JSON formatted 'read' action is supported.")
+  modified_rust = []
+  for read_url in action_dict.get(u"references",[]):
+    for rust in ReferenceUserStatus.objects.filter(has_been_read=False).select_related("ref").all():
+      if rust.ref.url == read_url:
+        print "found url: " + read_url
+        rust.has_been_read = True
+        modified_rust.append(rust)
+        break  
+  with transaction.commit_on_success():
+    for r in modified_rust:
+      r.save()
+  count = len(modified_rust)
+  response_dict = {u"action": u"read", u"status": u"success", u"count": count}
+  return HttpResponse(simplejson.dumps(response_dict), mimetype='application/json')
 
+@login_required(login_url='/accounts/login/')
+def user_river_sieve(request):
+  if request.method == 'GET':
+    return generate_user_sieve(request)
+  elif request.method == 'POST':
+    print "received: %s" % request
+    return apply_to_user_sieve(request)
+  else:
+    return HttpResponseNotAllowed(['GET','POST'])
+  
 @login_required(login_url='/accounts/login/')
 def user_river_sources(request):
   user_profile = request.user.userprofile
