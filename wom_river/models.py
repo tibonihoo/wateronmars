eee52b2ea56c14e0bb03f526ee75bd6b7573d1da
Thibauld Nion <thibauld@tibonihoo.net>
2013-05-05 22:23:56 +0200

    add ReferenceUserStatus to handle read/unread status

--- wom_river/models.py
+++ wom_river/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from wom_pebbles.models import Source
 from wom_pebbles.models import URL_MAX_LENGTH
 
+from django.contrib.auth.models import User
+
+from wom_pebbles.models import Reference
+
+
 class FeedSource(Source):
   """
   A source that is actually a a feed (typically RSS or Atom).
@@ -12,3 +17,19 @@ class FeedSource(Source):
   # Date marking the last time the source was checked for an update
   last_update = models.DateTimeField('last update')
 
+
+class ReferenceUserStatus(models.Model):
+  """
+  Grossly represents the "unread" flag: if no instance exists for a
+  given feed item, then the corresponding reference is unread, as it
+  is too if an instance exists zith the read flag set to false, but if
+  the read flag is set to true, then the user has read the reference.
+  """
+  # The reference that waits to be read
+  ref = models.ForeignKey(Reference)
+  # The user that is supposed to read it
+  user = models.ForeignKey(User)
+  # Repeat the publication date of the reference
+  ref_pub_date = models.DateTimeField('reference publication date')
+  # Read flag
+  has_been_read = models.BooleanField(default=False)
