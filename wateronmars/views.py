01d3e8cd31c94846f642c9a034b785dc10674190
Thibauld Nion <thibauld@tibonihoo.net>
2013-06-08 21:40:35 +0200

    facorize the generation of data necessary for all templates

--- wateronmars/views.py
+++ wateronmars/views.py
@@ -2,6 +2,21 @@ from django.http import HttpResponseRedirect
 from wom_river.tasks import collect_all_new_pebbles_sync
 from wom_river.tasks import delete_old_pebbles_sync
 
+def fill_base_data(username,title_qualify,d):
+  """Generate the context data needed for template that inherit from the base template.
+  'username': the user name of the  visitor's !
+  'title_qualify': the qualification of the page wrt the visitor (Your
+  or Public for now).
+  """
+  # TODO distinguish usernames as page_owner and visitor
+  d.update({
+    'username' : username,
+    'title_qualify': title_qualify,
+    'realm': "u/%s" % username
+    })
+  return d
+
+
 def home(request):
   return HttpResponseRedirect('/public/river')
 
