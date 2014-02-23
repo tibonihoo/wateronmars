624d29ce0f0cea84d9c55fb4124bba19ebdb2dfb
Thibauld Nion <thibauld@tibonihoo.net>
2014-02-23 22:45:51 +0100

    reduce the time threshold to 4 weeks to radically downsize the db

--- wom_user/settings.py
+++ wom_user/settings.py
@@ -31,7 +31,7 @@ from datetime import timedelta
 if hasattr(settings,"WOM_USER_NEWS_TIME_THRESHOLD"):
   NEWS_TIME_THRESHOLD = settings.WOM_USER_NEWS_TIME_THRESHOLD
 else:
-  NEWS_TIME_THRESHOLD = timedelta(weeks=12)
+  NEWS_TIME_THRESHOLD = timedelta(weeks=4)
 
 if hasattr(settings,"WOM_USER_MAX_ITEMS_PER_PAGE"):
   MAX_ITEMS_PER_PAGE = settings.WOM_USER_MAX_ITEMS_PER_PAGE
