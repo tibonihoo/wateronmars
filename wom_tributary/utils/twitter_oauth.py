323bb8d74281055535777e3479468e07d9a9aaee
Thibauld Nion <thibauld@tibonihoo.net>
2019-04-09 22:03:40 +0200

    Fix addition of the first twitter user timeline.

--- wom_tributary/utils/twitter_oauth.py
+++ wom_tributary/utils/twitter_oauth.py
@@ -44,6 +44,10 @@ def try_get_authorized_client(
   and a valid client if it has been granted."""
   register_acces_token_if_present(
     request_params, session, user_info)
+  if None in (
+      user_info.oauth_access_token,
+      user_info.oauth_access_token_secret ):
+    return None
   try:
     client = build_twitter_client(user_info)
     if client and client.get_actor() is not None:
