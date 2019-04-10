215f4baba2786ae7168e9ec17c0b63c824e67133
Thibauld Nion <thibauld@tibonihoo.net>
2019-04-10 23:20:04 +0200

    Keep track of the ugly utf8mb4 migration for MySQL

---
+++ doc/support_more_utf8_in_db.sql
@@ -0,0 +1,39 @@
+
+-- Truncated text (typically in post/pebble description) can be due to
+-- the db's encoding not supporting the full range of utf-8 characters
+-- (typically emojis) which requires changing the character set.
+--
+-- See https://mathiasbynens.be/notes/mysql-utf8mb4#column-index-length
+
+
+
+-- For each database:
+ALTER DATABASE tibonihoo_wom CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+--
+ALTER TABLE django_admin_log CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE django_admin_log MODIFY change_message longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE wom_classification_tag CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE wom_classification_tag MODIFY name varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE wom_pebbles_reference CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE wom_pebbles_reference MODIFY url varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE wom_pebbles_reference MODIFY title varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE wom_pebbles_reference MODIFY description longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE wom_river_webfeed CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE wom_river_webfeed MODIFY xmlURL varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE wom_tributary_generatedfeed CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE wom_tributary_generatedfeed MODIFY title varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE wom_user_userbookmark CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE wom_user_userbookmark MODIFY comment longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+
+REPAIR TABLE django_admin_log;
+OPTIMIZE TABLE django_admin_log;
+REPAIR TABLE wom_classification_tag;
+OPTIMIZE TABLE wom_classification_tag;
+REPAIR TABLE wom_pebbles_reference;
+OPTIMIZE TABLE wom_pebbles_reference;
+REPAIR TABLE wom_river_webfeed;
+OPTIMIZE TABLE wom_river_webfeed;
+REPAIR TABLE wom_tributary_generatedfeed;
+OPTIMIZE TABLE wom_tributary_generatedfeed;
+REPAIR TABLE wom_user_userbookmark;
+OPTIMIZE TABLE wom_user_userbookmark;
