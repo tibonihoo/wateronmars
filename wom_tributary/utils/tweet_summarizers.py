62a810356e875defef2b2de323dc738f813e739a
Thibauld Nion <thibauld@tibonihoo.net>
2021-05-11 22:03:51 +0200

    [Fix] Missing space in twitter summary

--- wom_tributary/utils/tweet_summarizers.py
+++ wom_tributary/utils/tweet_summarizers.py
@@ -160,7 +160,7 @@ def generate_basic_html_summary(
       continue
     doc_lines.append("<h3>#{}</h3>".format(tag))
     for t in sorted(tweets, key=lambda t:t.date):
-      doc_lines.append(f"<p><em>@{t.author}:</em><a href='{t.link}'>{t.content_summary}</a></p>")
+      doc_lines.append(f"<p><em>@{t.author}:</em> <a href='{t.link}'>{t.content_summary}</a></p>")
   doc_lines.append("")
   if not singular_topics_tweets:
     return "\n".join(doc_lines)
